78a13d588e6dd5b90d5fb6e1910e1c5cc793e598
xxxCoeNxxx <pedrocela1992@gmail.com>
2025-11-28 11:56:25 +0100

    hasta pertenencia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-// TRUE SI EMPIEZA CON ejemplo
+// EMPIEZA POR LA CADENA ^
 const ejemploLiteralOk = "ejemplos...";
 const ejemploLiteralNotOk = "xejemplo"
 const inicioLiteral = /^ejemplo/;
@@ -8,9 +8,9 @@ const inicioLiteral = /^ejemplo/;
 const resultLiteralOk = inicioLiteral.test(ejemploLiteralOk);
 console.log("ejemplo Inicio Ok: ", resultLiteralOk);
 const resultLiteralNotOk = inicioLiteral.test(ejemploLiteralNotOk);
-console.log("ejemplo Inicio NotOk: ", resultLiteralNotOk);
+console.log("ejemplo Inicio Not Ok: ", resultLiteralNotOk);
 
-// TRUE SI TERMINA CON ejemplo
+// TERMINA CON LA CADENA $ 
 const ejemploFinLiteralOk = "asejemplo";
 const ejemploFinLiteralNotOk = "ejemplos"
 const finLiteral = /ejemplo$/;
@@ -18,23 +18,36 @@ const finLiteral = /ejemplo$/;
 const resultFinLiteralOk = finLiteral.test(ejemploFinLiteralOk);
 console.log("ejemplo Fin OK: ", resultFinLiteralOk);
 const resultFinLiteralNotOk = finLiteral.test(ejemploFinLiteralNotOk);
-console.log("ejemplo Fin NotOk: ", resultFinLiteralNotOk);
+console.log("ejemplo Fin Not Ok: ", resultFinLiteralNotOk);
 
-// OMISION DE COMODINES
+// OMISION DE COMODINES \
 const ejemploOmisionComOk = "ejemplo.txt";
-const ejemploOmisionComNotOk = "ejemplo."
+const ejemploOmisionComNotOk = "ejemplo.";
 const omisionComodin = /ejemplo\./;
 
 console.log(`Omisión Comodín Ok: ${ejemploOmisionComOk} → ${omisionComodin.test(ejemploOmisionComOk)}`)
-console.log(`Omisión Comodín Ok: ${ejemploOmisionComNotOk} → ${omisionComodin.test(ejemploOmisionComNotOk)}`)
+console.log(`Omisión Comodín Not Ok: ${ejemploOmisionComNotOk} → ${omisionComodin.test(ejemploOmisionComNotOk)}`)
 
-// OMISION DE COMODINES ejemplo2
+// OMISION DE COMODINES ejemplo2 \
 const ejemploOmisionCom2Ok = "^ejemplo$.txt";
-const ejemploOmisionCom2NotOk = "asd^ejemplo$.txt"
+const ejemploOmisionCom2NotOk = "asd^ejemplo$.txt";
 const omisionComodin2 = /^\^ejemplo\$\./;
 
 console.log(`Omision Comodin 2 Ok: ${ejemploOmisionCom2Ok} → ${omisionComodin2.test(ejemploOmisionCom2Ok)}`);
-console.log(`Omision Comodin 2 NotOk: ${ejemploOmisionCom2NotOk} → ${omisionComodin2.test(ejemploOmisionCom2NotOk)}`);
+console.log(`Omision Comodin 2 Not Ok: ${ejemploOmisionCom2NotOk} → ${omisionComodin2.test(ejemploOmisionCom2NotOk)}`);
+
+// CUALQUIER CARACTER .
+const ejCualquierCaracterOk = "ejemplo.txt";
+const ejCualquierCaracterNotOk = "ejemplo.tx";
+const cualquierCaracter = /\....$/;
+
+console.log(`Cualquier caracter Ok: ${ejCualquierCaracterOk} → ${cualquierCaracter.test(ejCualquierCaracterOk)}`)
+console.log(`Cualquier caracter Not Ok: ${ejCualquierCaracterNotOk} → ${cualquierCaracter.test(ejCualquierCaracterNotOk)}`)
+
+// PERTENENCIA AL GRUPO []
+const valorGrupo = /[148]/;
+console.log(`Cualquiera de estos num "123" está incluído en ${valorGrupo}`, valorGrupo.test("123"));
+console.log(`Cualquiera de estos num "555" está incluído en ${valorGrupo}`, valorGrupo.test("555"));
 
 
 
